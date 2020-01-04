ce3d8b7ee40828b3e8dd36ba6aecca31a4e155a2
Collin <collinjoselyn@gmail.com>
2020-01-03 19:37:19 -0600

    added myReviews.php to the project

---
+++ myReviews.php
@@ -0,0 +1,104 @@
+<?php
+session_start();
+require_once 'dbconnection.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Reviews</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="style.css" rel="stylesheet">
+
+</head>
+
+
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">Reviews</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="movies.php">Movies</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="videoGames.php">Video Games</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="tv.php">TV</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <?php if(isset($_SESSION['username'])){
+           echo '<li class="nav-item">';
+           echo  '<a class="nav-link" href="signOut.php">' .'Sign out' .'</a>';
+           echo '</li>';}else{
+           echo '<li class="nav-item">';
+           echo  '<a class="nav-link" href="signIn.php">' .'Sign In' .'</a>';
+           echo '</li>';
+           } ?>
+           <?php 
+           if(isset($_SESSION['username'])){
+
+           }else{
+            echo '<li class="nav-item">';
+            echo '<a class="nav-link" href="createAccount.php">' .'Create Account' .'</a>';
+            echo '</li>'; }?>
+          <?php if(isset($_SESSION['username'])){
+            echo '<li class="nav-item">';
+            echo '<a class="nav-link" href="userHomePage.php">';
+            echo $_SESSION['username'] . '</a>' . '</li>'; }; ?>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Content -->
+  <?php if(!isset($_SESSION['username'])): header("location: signOut.php");?>
+<?php else: ?>
+<?php endif ?>
+  
+  
+<div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <?php echo "<h1> Reviews From " .$_SESSION['username']. "</h1>"; ?>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+
+
+
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.slim.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
+
+</body>
+
+</html>
